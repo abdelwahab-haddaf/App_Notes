2c848d8aa6a49012c6e967f018949bd7b349794f
Mohammed Walid <ana._.heek_ok@hotmail.com>
2019-12-22 10:12:56 +0200

    implement confirm email

---
+++ app/src/main/java/com/aug/noteapp/CheckYourEmailActivity.java
@@ -0,0 +1,15 @@
+package com.aug.noteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aug.noteapp.R;
+import android.os.Bundle;
+
+public class CheckYourEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_your_email);
+    }
+}
